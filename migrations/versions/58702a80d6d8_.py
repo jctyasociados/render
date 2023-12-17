ab7cfbd9ffbeadbd7008e5a67119b0f1d1e79fca
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-12-17 12:04:23 +0100

    CS50x Final Project V 1.0

--- migrations/versions/58702a80d6d8_.py
+++ migrations/versions/58702a80d6d8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8084df96ef64
+Revision ID: 58702a80d6d8
 Revises: 
-Create Date: 2023-12-14 14:52:46.835324
+Create Date: 2023-12-17 11:57:13.966854
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8084df96ef64'
+revision = '58702a80d6d8'
 down_revision = None
 branch_labels = None
 depends_on = None
